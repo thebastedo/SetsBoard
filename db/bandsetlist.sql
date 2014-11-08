64b140b1472c4d0f711d457b95b77d34deb3d41a
Cesar Lucas <jester2@gmail.com>
2014-11-08 21:48:25 +0000

    Added new columns to .sql file in db/bandsetlist.sql

--- db/bandsetlist.sql
+++ db/bandsetlist.sql
@@ -1,10 +1,13 @@
 /* 
-   bandsetlist.sql - version 0.0.1 - 11/6/2014 
+   bandsetlist.sql - version 0.0.2 - 11/8/2014 
    Schema file used to build the tables in the database
+	0.0.1 - Initial creation
+	0.0.2 - Added id(smallint(6) to setlist and songs tables) 
+		and setlistid(smallint(6)) songid(smallint(6))
 */
 
 begin;
-create table band (id smallint(6) primary key, name varchar(255));
-create table setlist(name varchar(255));
-create table songs(name varchar(255), duration time, status char(1));
+create table band (id smallint(6), name varchar(255), setlistid(smalling(6)));
+create table setlist(id smallint(6), name varchar(255), songid(smallint(6)));
+create table songs(id smallint(6), name varchar(255), duration time, status char(1));
 commit;
