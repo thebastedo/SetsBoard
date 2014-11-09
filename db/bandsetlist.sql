d6d7a8a61dd8925c15b53a724f1b4ff8d70a965a
Cesar Lucas <jester2@gmail.com>
2014-11-09 18:08:21 +0000

    Typo fixes in bandsetlist.sql

--- db/bandsetlist.sql
+++ db/bandsetlist.sql
@@ -3,11 +3,11 @@
    Schema file used to build the tables in the database
 	0.0.1 - Initial creation
 	0.0.2 - Added id(smallint(6) to setlist and songs tables) 
-		and setlistid(smallint(6)) songid(smallint(6))
+		and setlistid smallint(6) songid smallint(6)
 */
 
 begin;
-create table band (id smallint(6), name varchar(255), setlistid(smalling(6)));
-create table setlist(id smallint(6), name varchar(255), songid(smallint(6)));
+create table band (id smallint(6), name varchar(255), setlistid smallint(6));
+create table setlist(id smallint(6), name varchar(255), songid smallint(6));
 create table songs(id smallint(6), name varchar(255), duration time, status char(1));
 commit;
