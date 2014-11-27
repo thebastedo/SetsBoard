693fbdda57ef59ba539fc17a5c039e80c7c77ab0
Cesar Lucas <jester2@gmail.com>
2014-11-27 06:22:15 +0000

    Updated database parameters in bandsetlist.sql

--- db/bandsetlist.sql
+++ db/bandsetlist.sql
@@ -1,13 +1,15 @@
 /* 
-   bandsetlist.sql - version 0.0.2 - 11/8/2014 
+   bandsetlist.sql - version 0.0.3 - 11/9/2014 
    Schema file used to build the tables in the database
 	0.0.1 - Initial creation
 	0.0.2 - Added id(smallint(6) to setlist and songs tables) 
 		and setlistid smallint(6) songid smallint(6)
+	0.0.3 - Changed duration from time to int (number of seconds)
+	0.0.4 - added integer primary keys to id on tables, added songorder as a integer
 */
 
 begin;
-create table band (id smallint(6), name varchar(255), setlistid smallint(6));
-create table setlist(id smallint(6), name varchar(255), songid smallint(6));
-create table songs(id smallint(6), name varchar(255), duration time, status char(1));
+create table band (id integer primary key, name varchar(255), setlistid integer);
+create table setlist(id integer primary key, name varchar(255), songid integer, songorder integer);
+create table songs(id integer primary key, name varchar(255), duration int, status char(1));
 commit;
